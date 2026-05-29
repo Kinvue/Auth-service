a8cda8355ee9ebe3ca3e3bf790366f84390bfaa5
illya-klus <illyaklus2@gmail.com>
2026-05-30 02:10:03 +0300

    fix: transfer logic into modules/auth

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,15 @@
 import { Controller } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthResponse, LoginRequest } from '@kinvue/contracts/dist/gen/auth';
+import { AUTH_SERVICE_NAME } from '@kinvue/contracts/dist/gen/constants';
+import { GrpcMethod } from '@nestjs/microservices';
 
 @Controller()
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+    constructor(private readonly authService: AuthService) {}
+
+  @GrpcMethod(AUTH_SERVICE_NAME, 'Login')
+  public login (userCredentials : LoginRequest) : AuthResponse {
+    return this.authService.login(userCredentials);
+  }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,13 @@
+import { LoginRequest } from '@kinvue/contracts/dist/gen/auth';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    public login (userCredentials : LoginRequest) {
+        return {
+          accessToken: "access",
+          refreshToken: "refresh",
+          user: undefined,
+        }
+    }
+}
