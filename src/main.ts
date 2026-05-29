0219c75b0c9a5886423ac0ba7780b41e86cb5245
illya-klus <illyaklus2@gmail.com>
2026-05-30 02:30:27 +0300

    [IA-74] + additional endpoints log

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,17 @@ import { Transport } from '@nestjs/microservices';
 import { AUTH_V1_PACKAGE_NAME } from '@kinvue/contracts/dist/gen/auth';
 import { join } from 'path';
 import {AUTH_PROTO_PATH} from "@kinvue/contracts/dist/gen/constants"
+import { Logger, UseInterceptors } from '@nestjs/common';
+import { LoggerInterceptor } from './interseptors/logger.interseptor';
 
 
 async function bootstrap() {
+  const logger = new Logger("Main");
   const host = process.env.AUTH_GRPC_HOST ?? '0.0.0.0';
   const port = process.env.AUTH_GRPC_PORT ?? '50051';
 
+
+  logger.log("Setup Auth-service...");
   const app = await NestFactory.createMicroservice(AppModule, {
     transport: Transport.GRPC,
     options: {
@@ -18,7 +23,12 @@ async function bootstrap() {
       url: `${host}:${port}`,
     },
   });
+  logger.log("Setup Auth-service completed");
+
+  app.useGlobalInterceptors(new LoggerInterceptor())
 
   await app.listen();
+  logger.log(`Service started on: ${host}:${port}`);
+
 }
 bootstrap();
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Logger } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthResponse, LoginRequest } from '@kinvue/contracts/dist/gen/auth';
 import { AUTH_SERVICE_NAME } from '@kinvue/contracts/dist/gen/constants';
@@ -6,7 +6,9 @@ import { GrpcMethod } from '@nestjs/microservices';
 
 @Controller()
 export class AuthController {
-    constructor(private readonly authService: AuthService) {}
+    constructor(
+      private readonly authService: AuthService,
+    ) {}
 
   @GrpcMethod(AUTH_SERVICE_NAME, 'Login')
   public login (userCredentials : LoginRequest) : AuthResponse {
