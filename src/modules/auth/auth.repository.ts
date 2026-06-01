1cd9cf37010fbb9094d8d8e9bf45444f1bcdd959
illya-klus <illyaklus2@gmail.com>
2026-06-01 20:52:02 +0300

    add updateStatus method

--- src/modules/auth/auth.repository.ts
+++ src/modules/auth/auth.repository.ts
@@ -1,34 +1,36 @@
-import { UserPayload } from "@kinvue/contracts/dist/gen/auth";
-import { Injectable } from "@nestjs/common";
-import { AuthUser } from "generated/prisma/client";
-import { PrismaService } from "src/infrastructure/prisma/prisma.service";
-
-
-
-
-
+import { Injectable } from '@nestjs/common';
+import { AuthUser, AuthUserStatus, Prisma } from 'generated/prisma/client';
+import { PrismaService } from 'src/infrastructure/prisma/prisma.service';
 
 @Injectable()
 export class AuthRepository {
+  public constructor(private readonly prisma: PrismaService) {}
 
-    public constructor(
-        private readonly prisma : PrismaService
-    ){}
+  public async checkEmailCount(email: string): Promise<number> {
+    return await this.prisma.authUser.count({
+      where: { email },
+    });
+  }
 
-    public async checkEmailCount (email: string ) : Promise<number> {
-        return await this.prisma.authUser.count({
-            where:{ email }
-        });
-    }
-    public async findByEmail (email: string) : Promise <AuthUser | null>
-    {
-        return await this.prisma.authUser.findUnique({
-            where: {email}
-        })
-    }
-    public async createUser (data) {
-        return await this.prisma.authUser.create({
-            data
-        })
-    }
+  public async findByEmail(email: string): Promise<AuthUser | null> {
+    return await this.prisma.authUser.findUnique({
+      where: { email },
+    });
+  }
+
+  public async createUser(data: Prisma.AuthUserCreateInput): Promise<AuthUser> {
+    return await this.prisma.authUser.create({
+      data,
+    });
+  }
+
+  public async updateStatus(
+    id: string,
+    status: AuthUserStatus,
+  ): Promise<AuthUser> {
+    return await this.prisma.authUser.update({
+      where: { id },
+      data: { status },
+    });
+  }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,11 @@
-import { LoginRequest,RegisterRequest,RefreshRequest,LogoutRequest, AuthResponse , UserPayload } from '@kinvue/contracts/dist/gen/auth';
+import {
+  LoginRequest,
+  RegisterRequest,
+  RefreshRequest,
+  LogoutRequest,
+  AuthResponse,
+  UserPayload,
+} from '@kinvue/contracts/dist/gen/auth';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
@@ -7,152 +14,158 @@ import { AuthRepository } from './auth.repository';
 import { RpcException } from '@nestjs/microservices';
 import { status } from '@grpc/grpc-js';
 import { UserService } from '../user/user.service';
-
-
-
+import { AuthUserStatus } from 'generated/prisma/client';
 
 @Injectable()
 export class AuthService {
-    public constructor(
-        private readonly jwtService : JwtService,
-        private readonly config : ConfigService,
-        private readonly authRepository : AuthRepository,
-        private readonly userService : UserService
-    ){}
+  public constructor(
+    private readonly jwtService: JwtService,
+    private readonly config: ConfigService,
+    private readonly authRepository: AuthRepository,
+    private readonly userService: UserService,
+  ) {}
 
-    public async login (userCredentials : LoginRequest) : Promise<AuthResponse> {
-        const {email, password } = userCredentials;
+  public async login(userCredentials: LoginRequest): Promise<AuthResponse> {
+    const { email, password } = userCredentials;
 
-        const currentUser = await this.authRepository.findByEmail(email);
-        if(!currentUser) {
-            throw new RpcException({
-                code: status.UNAUTHENTICATED,
-                message: "Invalid credentials!"
-            })
-        }
-        const isPasswordValid = await this.comparePasswords(
-            password,
-            currentUser.passwordHash
-        );
-        if(!isPasswordValid){
-             throw new RpcException({
-                code: status.UNAUTHENTICATED,
-                message: "Invalid credentials!"
-            })
-        }
+    const currentUser = await this.authRepository.findByEmail(email);
 
-        //отримати юзера з USER service
-        const userData : UserPayload = {
-            id : currentUser.id,
-            email : currentUser.email,
-            role : currentUser.role, 
-            status : currentUser.status
-        } 
-        const secretForAccess = this.config.getOrThrow<string>("JWT_ACCESS_SECRET");
-        const secretForRefresh = this.config.getOrThrow<string>("JWT_REFRESH_SECRET");
-        
-        const refreshToken = await this.generateToken(userData, secretForRefresh, "30d");
-        const accessToken = await this.generateToken(userData, secretForAccess, "15m");
-
-        return {
-          accessToken: accessToken,
-          refreshToken: refreshToken,
-          user: userData,
-        }
+    if (!currentUser) {
+      throw new RpcException({
+        code: status.UNAUTHENTICATED,
+        message: 'Invalid credentials!',
+      });
     }
 
+    const isPasswordValid = await this.comparePasswords(
+      password,
+      currentUser.passwordHash,
+    );
 
-    public async register(userCredentials : RegisterRequest) :  Promise<AuthResponse>{
-        const {email, password , clientInfo} = userCredentials;
-
-        if (!clientInfo?.name) {
-            throw new RpcException({
-                code: status.INVALID_ARGUMENT,
-                message: "Name value must not be undefined"
-            })
-        }
-
-        //перевір чи є такий емейл
-        const usersWithThisEmail = await this.authRepository.checkEmailCount(email);
-        if(usersWithThisEmail) {
-            throw new RpcException({
-                code: status.ALREADY_EXISTS,
-                message: "User with this credentials already exists"
-            })
-        }
-
-        //хешуєш пароль
-        const hash = await this.hashPassword(password);
-
-        //створюєш юзера
-        const newUserData = {
-            email: email,
-            passwordHash: hash,
-        }
-        const user = await this.authRepository.createUser(newUserData)
-
-        await this.userService.createProfile({
-            authUserId: user.id,
-            username: clientInfo.name
-        })
-
-
-        //генеруємо токени
-        const userData : UserPayload = {
-            id : user.id,
-            email : user.email,
-            role : user.role, 
-            status : user.status
-        } 
-        const secretForAccess = this.config.getOrThrow<string>("JWT_ACCESS_SECRET");
-        const secretForRefresh = this.config.getOrThrow<string>("JWT_REFRESH_SECRET");
-
-        const refreshToken = await this.generateToken(userData, secretForRefresh, "30d");
-        const accessToken = await this.generateToken(userData, secretForAccess, "15m");
-
-        //повертаємо відповідь
-        return {
-            accessToken: accessToken,
-            refreshToken: refreshToken,
-            user: userData
-        } 
+    if (!isPasswordValid) {
+      throw new RpcException({
+        code: status.UNAUTHENTICATED,
+        message: 'Invalid credentials!',
+      });
     }
 
-    
-    public async logout(userCredentials : LogoutRequest) {
-        return 
+    if (currentUser.status !== 'ACTIVE') {
+      throw new RpcException({
+        code: status.FAILED_PRECONDITION,
+        message: 'Registration is not completed',
+      });
     }
 
-    public async refresh(dto: RefreshRequest) {
-  
-      return {
-        accessToken: "sdiufgsoid",
-        refreshToken : "sdiufgsoid",
-        user: undefined
-      };
-}
+    return this.issueTokens(currentUser);
+  }
 
-    private async hashPassword (password : string) {
-        return await hash(password, 10)
+  public async register(userCredentials: RegisterRequest): Promise<AuthResponse> {
+    const { email, password, clientInfo } = userCredentials;
+
+    if (!clientInfo?.name?.trim()) {
+      throw new RpcException({
+        code: status.INVALID_ARGUMENT,
+        message: 'Name value must not be undefined',
+      });
     }
-    private async comparePasswords (password : string , hash: string) : Promise<boolean> {
-        return await compare(password, hash)
+
+    const existingUser = await this.authRepository.findByEmail(email);
+
+    if (existingUser?.status === 'ACTIVE') {
+      throw new RpcException({
+        code: status.ALREADY_EXISTS,
+        message: 'User with this credentials already exists',
+      });
     }
-    private async verifyToken (tocken: string, secret: string) {
-        return await this.jwtService.verifyAsync<UserPayload>(
-            tocken,
-            { 
-                secret
-            }
-        )
+
+    if (existingUser?.status === 'PENDING') {
+      await this.userService.createProfile({
+        authUserId: existingUser.id,
+        username: clientInfo.name,
+      });
+
+      const activatedUser = await this.authRepository.updateStatus(
+        existingUser.id,
+        'ACTIVE',
+      );
+
+      return this.issueTokens(activatedUser);
     }
-    private async generateToken (payload: UserPayload, secret: string, expiresIn) {
-        return await this.jwtService.signAsync(
-            payload,
-            { 
-                secret ,
-                expiresIn,
-            }
-        )
-    }
+
+    const hash = await this.hashPassword(password);
+
+    const newUserData = {
+      email: email,
+      passwordHash: hash,
+      status: 'PENDING' as AuthUserStatus,
+    };
+
+    const user = await this.authRepository.createUser(newUserData);
+
+    await this.userService.createProfile({
+      authUserId: user.id,
+      username: clientInfo.name,
+    });
+
+    const activatedUser = await this.authRepository.updateStatus(
+      user.id,
+      'ACTIVE',
+    );
+
+    return this.issueTokens(activatedUser);
+  }
+
+  public async logout(userCredentials: LogoutRequest) {
+    return;
+  }
+
+  public async refresh(dto: RefreshRequest) {
+    return {
+      accessToken: 'sdiufgsoid',
+      refreshToken: 'sdiufgsoid',
+      user: undefined,
+    };
+  }
+
+  private async issueTokens(user): Promise<AuthResponse> {
+    const userData: UserPayload = {
+      id: user.id,
+      email: user.email,
+      role: user.role,
+      status: user.status,
+    };
+
+    const secretForAccess = this.config.getOrThrow<string>('JWT_ACCESS_SECRET');
+    const secretForRefresh = this.config.getOrThrow<string>('JWT_REFRESH_SECRET');
+
+    const refreshToken = await this.generateToken(userData, secretForRefresh, '30d');
+    const accessToken = await this.generateToken(userData, secretForAccess, '15m');
+
+    return {
+      accessToken,
+      refreshToken,
+      user: userData,
+    };
+  }
+
+  private async hashPassword(password: string) {
+    return await hash(password, 10);
+  }
+
+  private async comparePasswords(password: string, hash: string): Promise<boolean> {
+    return await compare(password, hash);
+  }
+
+  private async verifyToken(tocken: string, secret: string) {
+    return await this.jwtService.verifyAsync<UserPayload>(tocken, {
+      secret,
+    });
+  }
+
+  private async generateToken(payload: UserPayload, secret: string, expiresIn) {
+    return await this.jwtService.signAsync(payload, {
+      secret,
+      expiresIn,
+    });
+  }
 }
